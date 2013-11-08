1f8e6dc5e5e2bc55fc3e67af1416509c740a769e
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-11-08 11:59:24 +0100

    setup

---
+++ install.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+## install MCatNLO_Showering
+if [ -z "$CMSSW_BASE" ]; then
+  exit "set CMSSW_BASE or execute cmsenv"
+fi
+cd $CMSSW_BASE
+git clone git@github.com:fhoehle/MCatNLO_Showering.git
+cd MCatNLO_Showering
+if [ -f install.sh  ]; then
+  ./install.sh
+fi
+cd $CMSSW_BASE
+git clone git@github.com:fhoehle/MCatNLO2LHE.git
+cd MCatNLO2LHE
+if [ -f install.sh  ]; then
+  ./install.sh
+fi
+

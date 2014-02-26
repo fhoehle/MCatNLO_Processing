40602afb858a464938bbc82f12d1798cfdf98356
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-02-26 15:46:43 +0100

    new version

--- bin/mcatnlo2lhe2lheedm.sh
+++ bin/mcatnlo2lhe2lheedm.sh
@@ -1,44 +1,82 @@
 #! /bin/bash
-if [ "$1" == "-h" ]  || [ "$1" == "--help" ]; then
-    echo "Usage: `basename $0` [-h] [--onlyLHE]"
-    exit 0
-fi
-postfix="ttb"
+#######################
+## check if CMSSW environment is available and MCatNLO_Showering and MCatNLO2LHE are installed correctly
 if [ -z $CMSSW_BASE ] || [ ! -d $CMSSW_BASE/MCatNLO2LHE ] || [ ! -d $CMSSW_BASE/MCatNLO_Showering ]; then
-  echo "CMSSW_BASE missing i.e. cmsenv not executed or MCatNLO2LHE not installed"
+  echo "CMSSW_BASE missing i.e. cmsenv not executed or MCatNLO2LHE and or MCATNLO_Showering not installed"
   exit 1
 fi
-inputFile="Linux/$postfix.events"
-optionalInput=${BASH_ARGV[0]}
+### 
+function usage()
+{
+  echo -e "Script to convert mcatnlo output to lhe and edm.
+           Options:
+           --onlyLHE convert mcatnlo to lhe don't convert further to edm
+           --notest don't test resulting lhe
+           --input <inputMcatnlo>
+           --noMCatNLOinputs MCatNLO.inputs are not added to LHE header
+           --outputLHE <outputLHE file>
+           --outputEDM <outputEDM file>
+           -h print this help\n" 1>&2
+}
+##setup standard values of variables
+#### get input from MCatNLO.inputs
 if [ -f MCatNLO.inputs ]; then 
   postfix=`cat MCatNLO.inputs | grep EVPREFIX | sed 's/.*=\ *\([^=\ ]*\)[\ \$]*.*/\1/'`
-  if [ ! -f $inputFile ] && [ ! -f $optionalInput ]; then
-    echo "no mcatnlo output found: $inputFile"
-    exit 1
-  fi
-  else 
-    echo "not MCatNLO.inputs found"
+else 
+  echo "warning: no MCatNLO.inputs found, you should provide it or use input option"
+fi
+input="Linux/$postfix.events"
+filenamePrefix=${PWD##*/}
+outputLHE=${filenamePrefix}.lhe
+outputEDM=${filenamePrefix}_LHE2EDM
+## read from commandline
+while [ $# -ge 1 ]; do
+  case $1 in
+    --onlyLHE ) onlyLHE=true; shift ;;
+    --notest ) notest=true; shift ;;
+    --noMCatNLOinputs ) noMCatNLOinputs=true; shift ;;
+    --input ) input=$2; shift 2;;
+    --outputLHE ) outputLHE=$2; shift 2;;
+    --outputEDM ) outputEDM=$2; shift 2;;
+    -h ) usage 1>&2; exit 1 ;;
+    --help ) usage 1>&2; exit 1 ;;
+    * ) echo "$0: unrecognised option $1, use -h for help" 1>&2; exit 1 ;;
+  esac
+done
+### check input
+if [ ! -f $input ]; then
+    echo "input not found: $inputi is not existing"
     exit 1
 fi
-pwdName=${PWD##*/}
-echo "converting mcatnlo output to lhe"
-if [ ! -f $inputFile ]; then
-  inputFile=$optionalInput
-fi
-mcatnlo2lheCommand="cmsRun $CMSSW_BASE/MCatNLO2LHE/convertMCatNLO2LHE_cfg.py inputFiles=file:$inputFile mcatnloInputsFile=MCatNLO.inputs outputFile=$pwdName.lhe >& converting_mcatnlo2edm_${pwdName}_log.txt"
+## MCatNLO to LHE
+mcatnlo2lheCommand="cmsRun $CMSSW_BASE/MCatNLO2LHE/convertMCatNLO2LHE_cfg.py inputFiles=file:$input mcatnloInputsFile=MCatNLO.inputs outputFile=$outputLHE >& converting_mcatnlo2edm_${filenamePrefix}_log.txt"
 echo "$mcatnlo2lheCommand"
 eval $mcatnlo2lheCommand
 #renaming
-mv $pwdName.lhe.root $pwdName.lhe
-## adding mcatnlo.inputs
-cmd="python $CMSSW_BASE/MCatNLO2LHE/addMCatNLOinputsToLHEfile.py --lheFile $pwdName.lhe --mcatnloInputs MCatNLO.inputs"
-echo $cmd
-eval $cmd
-# LHE2EDM
-if [[ "$1" == "--onlyLHE" ]]; then
-  exit 0 
+mv ${outputLHE}.root $outputLHE
+#
+if [ "x$notest" != "xtrue" ]; then
+  echo "testing lhe"
+  testLHEcmd="cmsDriver.py lhetest --filein file:$outputLHE --mc --conditions auto:startup -n -1 --python lhetest_${filenamePrefix}.py --step NONE --no_output  >& lhetest_${filenamePrefix}.py_log.txt"
+  echo "$testLHEcmd"
+  eval $testLHEcmd
 fi
-echo "converting from LHE 2 EDM"
-lhe2edmCommand="cmsRun $CMSSW_BASE/MCatNLO_Showering/TT_mcatnlo_LHE2EDM_cfg.py inputFiles=file:$pwdName.lhe outputFile=${pwdName}_LHE2EDM >& lhe2edm_${pwdName}_log.txt"
-echo "$lhe2edmCommand"
-eval $lhe2edmCommand
+## adding mcatnlo.inputs
+if [ "x$noMCatNLOinputs" != "xtrue" ]; then 
+  cmd="python $CMSSW_BASE/MCatNLO2LHE/addMCatNLOinputsToLHEfile.py --lheFile $outputLHE --mcatnloInputs MCatNLO.inputs"
+  echo $cmd
+  eval $cmd
+  if [ "x$notest" != "xtrue" ]; then
+    echo "testing lhe"
+    testLHEcmd="cmsDriver.py lhetest --filein file:$outputLHE --mc --conditions auto:startup -n -1 --python lhetest_${filenamePrefix}.py --step NONE --no_output >& lhetest_${filenamePrefix}.py_withMCatnlo.inputs_log.txt "
+    echo "$testLHEcmd"
+    eval $testLHEcmd
+  fi
+fi
+# LHE2EDM
+if [ "x$onlyLHE" != "xtrue" ]; then
+  echo "converting from LHE 2 EDM"
+  lhe2edmCommand="cmsRun $CMSSW_BASE/MCatNLO_Showering/TT_mcatnlo_LHE2EDM_cfg.py inputFiles=file:$outputLHE outputFile=$outputEDM >& ${filenamePrefix}_LHE2EDM_cfg.py_log.txt"
+  echo "$lhe2edmCommand"
+  eval $lhe2edmCommand
+fi
